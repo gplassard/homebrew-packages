70ed6b34c66546ac8a98c28eec39f280fe8eb708
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2024-07-06 01:40:42 +0200

    homebrew

---
+++ jaw.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Jaw < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/gplassard/jaw/releases/download/v0.0.2/jaw-0.0.2.zip"
+  sha256 "2a82e201e3357a50086fac7b0ecebd5806edd3f6ec6e7738edb95432fd7c7dd2"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    bin.install "bin/jaw"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test jaw`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
