2dc751f4c979880b99be002c4f7bf04d61372f90
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2025-02-06 23:36:31 +0100

    feat(ssm-env): upgrade ssm env

--- ssm-env.rb
+++ ssm-env.rb
@@ -4,8 +4,8 @@
 class SsmEnv < Formula
   desc ""
   homepage ""
-  url "https://github.com/gplassard/ssm-env/releases/download/v0.1.5/ssm-env-x86_64-apple-darwin"
-  sha256 "320289dc2cd9ca27cc0cf76be6928a7203483ccd18d9e57d9c928e4efc9387c8"
+  url "https://github.com/gplassard/ssm-env/releases/download/v0.2.1/ssm-env-x86_64-apple-darwin"
+  sha256 "32e16da0afc6d2d5a4d046fb0a46869870391c9175144e6a42f1b0232bfefa2e"
   license ""
 
   def install
