852a571b9dd4b5a602a9b99f514382ab4ddcc419
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2025-12-20 15:01:34 +0100

    Update URL and SHA256 for ssm-env to version 0.3.8

--- ssm-env.rb
+++ ssm-env.rb
@@ -4,8 +4,8 @@
 class SsmEnv < Formula
   desc ""
   homepage ""
-  url "https://github.com/gplassard/ssm-env/releases/download/v0.2.2/ssm-env-x86_64-apple-darwin"
-  sha256 "35cf768928e13cf04474f4db5991499ae80e6c44f1943b338c53c14fd24b9ac6"
+  url "https://github.com/gplassard/ssm-env/releases/download/v0.3.8/ssm-env-x86_64-apple-darwin"
+  sha256 "sha256:c8506057de46fd4b48786f5855622d24106b017b742e4b2e2681e0bc2a933605"
   license ""
 
   def install
