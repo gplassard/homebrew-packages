9790450fbe030781fbe97b12660b7b783d4d7aeb
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2025-12-20 15:03:59 +0100

    Fix sha256 hash formatting in ssm-env.rb

--- ssm-env.rb
+++ ssm-env.rb
@@ -5,7 +5,7 @@ class SsmEnv < Formula
   desc ""
   homepage ""
   url "https://github.com/gplassard/ssm-env/releases/download/v0.3.8/ssm-env-x86_64-apple-darwin"
-  sha256 "sha256:c8506057de46fd4b48786f5855622d24106b017b742e4b2e2681e0bc2a933605"
+  sha256 "c8506057de46fd4b48786f5855622d24106b017b742e4b2e2681e0bc2a933605"
   license ""
 
   def install
