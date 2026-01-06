0d545673041d6418ffc8d1b33ba5e6cd644f4737
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2026-01-06 21:26:30 +0100

    chore(woof): upgrade woof

--- woof.rb
+++ woof.rb
@@ -6,11 +6,11 @@ class Woof < Formula
   homepage ""
 
   if Hardware::CPU.arm?
-    url "https://github.com/gplassard/woof/releases/download/v0.0.5/woof_Darwin_arm64.tar.gz"
-    sha256 "ea57215bcdb76905423d54a002c57f82b1e7ccc4ae89f68698dcc0c9a4332fb3"
+    url "https://github.com/gplassard/woof/releases/download/v0.0.6/woof_Darwin_arm64.tar.gz"
+    sha256 "bf9055d1407dcd0d7724936f5bdc4fee741bbbb6c3787dcbf602c826da0f002c"
   else
-    url "https://github.com/gplassard/woof/releases/download/v0.0.5/woof_Darwin_x86_64.tar.gz"
-    sha256 "c8da12e5f6079b950c2fa62ea8b5fa8ced5418089ccf3b9bc94351c49a575e27"
+    url "https://github.com/gplassard/woof/releases/download/v0.0.6/woof_Darwin_x86_64.tar.gz"
+    sha256 "3202023a2b5fa37e18e5cbaee3027cac53cdb97867e3d29928777c60d968855d"
   end
 
   license ""
